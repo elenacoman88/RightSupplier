a581ff077bcc1bbf0a73e70ea5661e0b43851f6d
elenacoman88 <ella_kity@yahoo.com>
2026-02-25 15:42:09 +0200

    Create BlogPost model

---
+++ Models/BlogPost.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RightSupplier.Models
+{
+    public class BlogPost
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
